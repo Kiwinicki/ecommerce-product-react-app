9c563f42aad9c0570ae7112cfce5e9ead86fa105
DESKTOP-2P9DAPD\Windows 10 <j.kiwinicki@wp.pl>
2021-10-20 19:51:14 +0200

    working mobile gallery

--- src/components/organisms/ProductGallery.js
+++ src/components/organisms/ProductGallery.js
@@ -1,5 +1,10 @@
+import { useState } from 'react';
 import styled from 'styled-components';
 
+import { BREAKPOINTS, COLORS } from 'utils/STYLE_VARIABLES';
+
+import nextIcon from 'images/icon-next.svg';
+import prevIcon from 'images/icon-previous.svg';
 import imageThumbnail1 from 'images/image-product-1-thumbnail.jpg';
 import imageThumbnail2 from 'images/image-product-2-thumbnail.jpg';
 import imageThumbnail3 from 'images/image-product-3-thumbnail.jpg';
@@ -8,7 +13,6 @@ import image1 from 'images/image-product-1.jpg';
 import image2 from 'images/image-product-2.jpg';
 import image3 from 'images/image-product-3.jpg';
 import image4 from 'images/image-product-4.jpg';
-import { BREAKPOINTS } from 'utils/STYLE_VARIABLES';
 
 const images = [image1, image2, image3, image4];
 const imagesThumbnails = [imageThumbnail1, imageThumbnail2, imageThumbnail3, imageThumbnail4];
@@ -16,15 +20,29 @@ const imagesThumbnails = [imageThumbnail1, imageThumbnail2, imageThumbnail3, ima
 const screenWidth = document.body.clientWidth;
 
 const ProductGallery = () => {
+	const [currImgIndex, setCurrImgIndex] = useState(0);
+
+	const changeToNextImage = () => {
+		setCurrImgIndex((prev) => (prev === images.length - 1 ? 0 : prev + 1));
+	};
+
+	const changeToPrevImage = () => {
+		setCurrImgIndex((prev) => (prev === 0 ? images.length - 1 : prev - 1));
+	};
+
 	return (
 		<GalleryContainer>
 			<FullResolutionImage>
-				<img src={images[0]} alt="product" />
+				<GalleryNavBtn prev onClick={changeToPrevImage} />
+				<img src={images[currImgIndex]} alt="product" />
+				<GalleryNavBtn next onClick={changeToNextImage} />
 			</FullResolutionImage>
 			{screenWidth >= BREAKPOINTS.desktop && <ThumbnailsImagesList />}
 		</GalleryContainer>
 	);
 };
+
+export default ProductGallery;
 
 const ThumbnailsImagesList = () => {
 	return (
@@ -36,9 +54,23 @@ const ThumbnailsImagesList = () => {
 	);
 };
 
-export default ProductGallery;
-
 const GalleryContainer = styled.div``;
+
+const GalleryNavBtn = styled.button`
+	background-color: ${COLORS.lightGrayishBlue};
+	background-image: url(${({ prev, next }) => (next ? nextIcon : prev ? prevIcon : '')});
+	background-repeat: no-repeat;
+	background-position: center;
+	background-size: 10px;
+	width: 38px;
+	height: 38px;
+	position: absolute;
+	transform: translateY(-50%);
+	top: 50%;
+	${({ prev, next }) => (next ? 'right: 1rem;' : prev ? 'left: 1rem;' : '')}
+	border-radius: 50%;
+	border: none;
+`;
 
 const ThumbnailsImagesContainer = styled.div`
 	display: flex;
@@ -54,9 +86,10 @@ const ThumbnailImage = styled.img.attrs(({ src, alt }) => ({
 `;
 
 const FullResolutionImage = styled.div`
+	position: relative;
 	overflow-y: hidden;
 	height: 300px;
-	img {
+	& > img {
 		object-fit: cover;
 		width: 100vw;
 		height: 100%;
