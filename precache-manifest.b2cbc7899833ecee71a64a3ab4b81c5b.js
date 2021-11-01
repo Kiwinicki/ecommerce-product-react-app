53b9d30983945c39ba67a4d00abd5b014c015f41
DESKTOP-2P9DAPD\Windows 10 <j.kiwinicki@wp.pl>
2021-11-01 17:17:08 +0100

    Updates

---
+++ precache-manifest.b2cbc7899833ecee71a64a3ab4b81c5b.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4688f4ad1c5a9097c2cc352052ac4fca",
+    "url": "/ecommerce-product-react-app/index.html"
+  },
+  {
+    "revision": "b75214e873ab4ec45b27",
+    "url": "/ecommerce-product-react-app/static/js/2.a13530b7.chunk.js"
+  },
+  {
+    "revision": "9f1c223dde5fe34750ffa3f0e44a974d",
+    "url": "/ecommerce-product-react-app/static/js/2.a13530b7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "abfd369d9085621e4bd4",
+    "url": "/ecommerce-product-react-app/static/js/3.d97837f4.chunk.js"
+  },
+  {
+    "revision": "35c471ad9981241f7ddc",
+    "url": "/ecommerce-product-react-app/static/js/main.d2d38a15.chunk.js"
+  },
+  {
+    "revision": "552c5da6f9ca91e24e78",
+    "url": "/ecommerce-product-react-app/static/js/runtime-main.eba16a0d.js"
+  },
+  {
+    "revision": "369779e8ca780747dcde84c93d1ead72",
+    "url": "/ecommerce-product-react-app/static/media/icon-cart.369779e8.svg"
+  },
+  {
+    "revision": "b0321cb3e6bf6aef8ecc31e32a577687",
+    "url": "/ecommerce-product-react-app/static/media/icon-delete.b0321cb3.svg"
+  },
+  {
+    "revision": "2166e08d2f67f82606d57886e65f367c",
+    "url": "/ecommerce-product-react-app/static/media/icon-menu.2166e08d.svg"
+  },
+  {
+    "revision": "0035f16e8254ffaef52d8039f515581a",
+    "url": "/ecommerce-product-react-app/static/media/icon-minus.0035f16e.svg"
+  },
+  {
+    "revision": "d13f859f0bc3a337e36dd0c85251c30c",
+    "url": "/ecommerce-product-react-app/static/media/icon-plus.d13f859f.svg"
+  },
+  {
+    "revision": "0ac65e8df3cdb3240e82aac50d5b84c5",
+    "url": "/ecommerce-product-react-app/static/media/image-product-1.0ac65e8d.jpg"
+  },
+  {
+    "revision": "dc5f56d918a4a172b00d9fb5c782f873",
+    "url": "/ecommerce-product-react-app/static/media/image-product-2.dc5f56d9.jpg"
+  },
+  {
+    "revision": "d8f0fd258d7833d69417c1cc12d217bb",
+    "url": "/ecommerce-product-react-app/static/media/image-product-3.d8f0fd25.jpg"
+  },
+  {
+    "revision": "5e95fcb499ff1a1c449b25f41fc5bf1b",
+    "url": "/ecommerce-product-react-app/static/media/image-product-4.5e95fcb4.jpg"
+  },
+  {
+    "revision": "06305c96f6e5bbef5501440e19da4d62",
+    "url": "/ecommerce-product-react-app/static/media/logo.06305c96.svg"
+  }
+]);
